dfc9b73e56578e9c17a7a3370506d28219c476bf
waqas831 <waqasishaq800@gmail.com>
2023-02-22 11:42:13 +0500

    first commit

---
+++ components/Passwordgenerator/PasswordGenerator.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const PasswordGenerator = () => {
+  return (
+    <View>
+      <Text>PasswordGenerator</Text>
+    </View>
+  );
+};
+
+export default PasswordGenerator;
+
+const styles = StyleSheet.create({});
